9e0603907313c0fd419fd671fdbdd4d7269d0c09
jorge.perez01epn <jorge.perez01epn@gmail.com>
2024-06-27 21:28:01 +0000

    PDF update

--- src/utils/generateCertificatePDF.js
+++ src/utils/generateCertificatePDF.js
@@ -4,46 +4,27 @@ const puppeteer = require('puppeteer');
 const locateChrome = require('locate-chrome');
 const htmlTemplate = require('./html_template');
 
-// const logFilePath = path.join(__dirname, 'logfile.txt');
-// function writeToLog(message) {
-//     fs.appendFileSync(logFilePath, `${new Date().toISOString()} - ${message}\n`);
-// }
-// writeToLog('Iniciando script...');
-// console.log = (message) => {
-//     writeToLog(message); // Redirige los logs a tu archivo
-//     process.stdout.write(`${message}\n`); // También imprime en la consola estándar
-// };
-// console.error = (message) => {
-//     writeToLog(`[ERROR] ${message}`);
-//     process.stderr.write(`[ERROR] ${message}\n`);
-// };
-
 async function generateCertificatePDF(name, documentIdentification, course, description, issuedAt, tokenId, transactionHash, academicAddress) {
     try {
         let htmlTemplateCopy = htmlTemplate;
 
         const course_l = course;
-        // const htmlTemplatePath = './src/utils/certificate-template.html';
-        // let htmlTemplate = await fs.readFileSync(htmlTemplatePath, 'utf-8');
-        // const cssFilePath = './src/utils/styles.css';
-        // const cssStyles = await fs.readFile(cssFilePath, 'utf-8');
+        const issuedDate = new Date(issuedAt);
+        const day = issuedDate.getDate().toString().padStart(2, '0');
+        const month = (issuedDate.getMonth() + 1).toString().padStart(2, '0'); // El mes se indexa desde 0, por lo tanto, se suma 1.
+        const year = issuedDate.getFullYear();
 
-        // Insertar estilos CSS en el HTML (dentro de la etiqueta <style>)
-        // htmlTemplate = htmlTemplate.replace('</head>', `<style>${cssStyles}</style></head>`);
-        // Replace placeholders with actual data
+        const formattedDate = `${day}/${month}/${year}`;
+        // const formattedDate = issuedDate.toISOString().split('T')[0];
+
         htmlTemplateCopy = htmlTemplateCopy.replace('{{name}}', name);
         htmlTemplateCopy = htmlTemplateCopy.replace('{{documentIdentification}}', documentIdentification);
         htmlTemplateCopy = htmlTemplateCopy.replace('{{course}}', course);
         htmlTemplateCopy = htmlTemplateCopy.replace('{{course2}}', course_l);
         htmlTemplateCopy = htmlTemplateCopy.replace('{{description}}', description);
-        htmlTemplateCopy = htmlTemplateCopy.replace('{{issuedAt}}', issuedAt);
+        htmlTemplateCopy = htmlTemplateCopy.replace('{{issuedAt}}', formattedDate);
         htmlTemplateCopy = htmlTemplateCopy.replace('{{tokenId}}', tokenId);
         htmlTemplateCopy = htmlTemplateCopy.replace('{{transactionHash}}', transactionHash);
-
-        // const backgroundPath = './src/utils/img/background.png';
-        // const backgroundImageBytes = await fs.readFile(backgroundPath);
-        // const backgroundBase64 = backgroundImageBytes.toString('base64');
-        // htmlTemplate = htmlTemplate.replace('{{Logo}}', backgroundBase64);
 
         const url = `https://sepolia.etherscan.io/nft/${academicAddress}/${tokenId}`;
         const transactionHashQRCode = await QRCode.toDataURL(url);
@@ -53,7 +34,6 @@ async function generateCertificatePDF(name, documentIdentification, course, desc
         htmlTemplateCopy = htmlTemplateCopy.replace('{{url-hash}}', url);
 
         // Generate PDF using Puppeteer
-        // const browser = await puppeteer.launch();
         let executablePath = await new Promise(resolve => locateChrome((arg) => resolve(arg))) || '';
 
         const browser = await puppeteer.launch({
@@ -112,15 +92,7 @@ async function generateCertificatePDF(name, documentIdentification, course, desc
 
         await page.setViewport({ width: 1920, height: 1080 });
         await page.setContent(htmlTemplateCopy, { waitUntil: 'networkidle0' });
-        // await page.addStyleTag({ path: './src/utils/styles.css' });
         await page.emulateMediaType('screen');
-        // await page.screenshot({
-        //     path: './src/utils/screenshot.jpg',
-        //     fullPage: true,
-        //     quality: 100,
-        //     encoding: 'binary',
-        //     type: 'jpeg',
-        //   });
 
         const pdfOptions = {
             landscape: true, // Establecer formato horizontal
--- src/utils/html_template.js
+++ src/utils/html_template.js
@@ -1,14 +1,4 @@
-let args = [
-    '--no-sandbox',
-    '--disable-setuid-sandbox',
-  ];
-
-
-let options = {
-      format: 'A5',
-      printBackground: true,
-      preferCSSPageSize: true
-    };
+//  JS file for html PDF template
 
 const html_template = `
 <!DOCTYPE html>
@@ -58,15 +48,16 @@ const html_template = `
     
             .qr-code {
                 display: flex;
+                flex-direction: column;
                 justify-content: start;
-                margin-top: -3rem;
+                margin-top: -2.8rem;
                 margin-left: 1rem;
             }
     
             .name-participant-section {
                 /* background: red; */
                 margin-top: 22rem;
-                margin-left: 23rem;
+                margin-left: 22rem;
                 /* border: #28348A solid; */
                 max-width: 50%;
             }
@@ -111,23 +102,37 @@ const html_template = `
             .hash-style:active {
                 color: #28348A; /* Color del enlace al hacer clic */
             }
+            .pill {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                width: auto;
+                margin-top: 35px;
+                height: 40px;
+                background: #04AA6D;
+                border-radius: 999em 999em 999em 40px;
+                
+            }
         </style>
 </head>
 
 <body style="margin: 0">
     
     <div class="page-background"></div>
-    <div id="bodydiv">
-        <div class="name-participant-section">
-            <h1 class="participant-name"><strong>{{name}}</strong></h1>
-            <p>Por haber aprobado el curso {{course}}: {{description}}.</p>
-            <p class="course-name"><strong>{{course2}}</strong></p>
-            <p><strong>Fecha:</strong> {{issuedAt}}</p>
-            <p><strong>Transaction Hash:</strong></p>
-            <a href="{{url-hash}}" class="hash-style">{{transactionHash}}</a>
+        <div id="bodydiv">
+            <div class="name-participant-section">
+                <h1 class="participant-name"><strong>{{name}}</strong></h1>
+                <p class=".playball-regular">Por haber aprobado el curso <strong>{{course}}</strong>: {{description}}.</p>
+                <!-- <p class="course-name"><strong>{{course2}}</strong></p> -->
+                <p class=".playball-regular">Emitido el {{issuedAt}}</p>
+                <p class=".playball-regular"><strong>Transaction Hash:</strong></p>
+                <a href="https://console.cloudinary.com/console/c-dbe373d6f0e0210e4726b1a020abd8/media_library/folders/c82b480d1a892b94ed34f5d4ade8efb553?view_mode=mosaic" class="hash-style">0xeeeb180c9788e1758bb3af0f152d683ba6ccc40053dc0a27519f24f0649729</a>
+                <div class="pill">
+                    <p class=".playball-regular" style="font-size: small;color: white;"><strong>Certificado NFT registrado en la Blockchain</strong></p>  
+                </div>
         </div>
         <div class="qr-code">
-            <img style="max-width: 80px; border: white solid 5px;border-radius: 5%;" src="data:image/png;base64,{{transactionHashQRBase64}}" alt="Transaction Hash QR Code">
+            <img style="max-width: 75px; border: white solid 5px;border-radius: 5%;" src="data:image/png;base64,{{transactionHashQRBase64}}" alt="Transaction Hash QR Code">
         </div>
     </div>
 </body>
